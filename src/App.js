f2cc931109dd604c96f352c4ee48aebf0c2c6360
Dario Rodrigo Torres Messenzani <54510225+Rodrigo00909@users.noreply.github.com>
2021-04-22 03:10:24 -0300

    structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import {useState, useEffect} from 'react';
+import Header from './components/Header/Header';
+
+import {Api} from './api/Api';
 
 import './App.css';
 
 function App() {
+
+  const [animes,SetAnimes] = useState([]);
+
+  const [topAnimes, setTopAnimes] = useState([]);
+
+  const [search, setSearch] = useState([]);
+
   return (
     <>
-      <main>
-        <h1>Hello world</h1>
-      </main>
+      <Header />
+      <div>
+        
+      </div>
     </>
   );
 }
---
+++ src/api/Api.jsx
@@ -0,0 +1,3 @@
+const url = 'https://api.jikan.moe/v3/top/type/page/subtype';
+
+export const Api = url;
