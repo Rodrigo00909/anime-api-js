44e7a0d7fd2c24063020c260a5f3595fe0e36ba5
Dario Rodrigo Torres Messenzani <54510225+Rodrigo00909@users.noreply.github.com>
2021-04-25 17:24:32 -0300

    favorite animes functionality

--- src/App.js
+++ src/App.js
@@ -6,18 +6,21 @@ import Content from './components/Content/Content';
 
 import './App.css';
 
-/* const url = 'https://api.jikan.moe/v3/top/type/page/subtype'; */
+const url = 'https://api.jikan.moe/v3/';
 
 function App() {
 
-  const [animes,SetAnimes] = useState([]);
+  const [animes, setAnimes] = useState([]);
 
   const [topAnimes, setTopAnimes] = useState([]);
 
+  const [favoriteAnimes, setFavoriteAnimes] = useState([]);
+
   const [search, setSearch] = useState([]);
 
+  // Get top Animes
   const GetTopAnimes = async () => {
-    const temp = await fetch(`https://api.jikan.moe/v3/top/anime/1/bypopularity`).then(res => res.json());
+    const temp = await fetch(`${url}top/anime/1/bypopularity`).then(res => res.json());
     
     setTopAnimes(temp.top.slice(0, 5));
   }
@@ -25,13 +28,25 @@ function App() {
   useEffect(() => {
     GetTopAnimes();
 
-    console.log(topAnimes);
-  }, [])
+  }, []);
+
+  // Get Animes
+  const GetFavoriteAnimes = async () => {
+    const temp = await fetch(`${url}top/anime/1/airing`).then(res => res.json());
+
+    setFavoriteAnimes(temp.top.slice(0, 10));
+  }
+
+  useEffect(() => {
+    GetFavoriteAnimes();
+
+  }, []);
+
 
   return (
     <div className="App">
       <Header />
-      <Content topAnimes={topAnimes}/>
+      <Content topAnimes={topAnimes} favoriteAnimes={favoriteAnimes}/>
     </div>
   );
 }
--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,25 +1,40 @@
 import React from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
+import GridList from '@material-ui/core/GridList';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        flexGrow: 1,
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        overflow: 'hidden',
     },
-    paper: {
-        padding: theme.spacing(2),
+    gridList: {
+        width: 'auto',
+        height: 'auto',
+    },
+    mainContent: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+    },
+    sidebar: {
         textAlign: 'center',
-        color: theme.palette.text.secondary,
-        backgroundColor: '#2d3338'
+        display: 'flex',
+        alignItems: 'center'
+    },
+    img: {
+        width: '150px',
+        height: '150px'
     },
 }));
 
 
-function Content(topAnimes) {
+function Content({ topAnimes, favoriteAnimes }) {
 
     const classes = useStyles();
 
@@ -29,11 +44,30 @@ function Content(topAnimes) {
                 <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }}>
                     <Grid container spacing={3}>
                         <Grid item xs={12} sm={8}>
-                            <Paper className={classes.paper}>xs=12 sm=6</Paper>
+                            <h3>Animes Esperados</h3>
+                            {favoriteAnimes.map(anime => (
+                                <Grid item xs={6} sm={6} className={classes.mainContent}>
+                                    <Grid item xs={12} sm={12}>
+                                        <div><img className={classes.img} src={anime.image_url} alt="" /></div>
+                                    </Grid>
+                                    <Grid item xs={12} sm={12}>
+                                        <a href={anime.url} key={anime.mal_id} target="_blank" rel="noreferrer">{anime.title}</a>
+                                    </Grid>
+                                </Grid>
+                            ))}
                         </Grid>
                         <Grid item xs={12} sm={4}>
-                            <h3>Animes Destacados</h3>
-                            <a href="#" target="_blank" rel="noreferrer">Shingeki No Kyojin</a>
+                            <h3>Animes Populares de siempre</h3>
+                            {topAnimes.map(anime => (
+                                <div className={classes.sidebar} >
+                                    <Grid item>
+                                        <div><img className={classes.img} src={anime.image_url} alt="" /></div>
+                                    </Grid>
+                                    <Grid item>
+                                        <a href={anime.url} key={anime.mal_id} target="_blank" rel="noreferrer">{anime.title}</a>
+                                    </Grid>
+                                </div>
+                            ))}
                         </Grid>
                     </Grid>
                 </Typography>
